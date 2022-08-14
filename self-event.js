f76c1895ea86a2be61b97064b47524259ce77a5f
shubhoahmed <shubhoahmed03@gmail.com>
2022-08-14 18:22:22 +0600

    self practice

---
+++ self-event.js
@@ -0,0 +1,16 @@
+document.getElementById('make-red').addEventListener('mouseover', function () {
+    document.body.style.backgroundColor = 'red';
+});
+
+document.getElementById('make-yellow').addEventListener('mouseout', function () {
+    document.body.style.backgroundColor = 'yellow';
+});
+document.getElementById('black').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'black';
+});
+document.getElementById('green').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'green';
+});
+document.getElementById('pink').addEventListener('mouseup', function () {
+    document.body.style.backgroundColor = 'pink';
+})
